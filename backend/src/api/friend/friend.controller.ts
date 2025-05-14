ef9ad49a6a36cfabb47dc5e0bac129317e9fcfbb
Kurucz Laci <lacikurucz09@gmail.com>
2025-05-14 21:26:09 +0200

    barátok végpontok

--- backend/src/api/friend/friend.controller.ts
+++ backend/src/api/friend/friend.controller.ts
@@ -2,7 +2,15 @@
 https://docs.nestjs.com/controllers#controllers
 */
 
-import { Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  UseGuards,
+} from '@nestjs/common';
 import { FriendService } from './friend.service';
 import { AuthGuard } from '../auth/guard/auth.guard';
 import { AuthModel, User } from '../auth/decorator/auth.decorator';
@@ -12,8 +20,26 @@ export class FriendController {
   constructor(private readonly friendService: FriendService) {}
 
   @UseGuards(AuthGuard)
+  @Get('user/friend')
+  getMyFriends(@User() user: AuthModel) {
+    return this.friendService.getMyFriends(user.sub);
+  }
+
+  @UseGuards(AuthGuard)
   @Post('user/friend/:id')
   requestFriend(@User() user: AuthModel, @Param('id') receiverId: string) {
     return this.friendService.requestFriend(user.sub, parseInt(receiverId));
+  }
+
+  @UseGuards(AuthGuard)
+  @Put('user/friend/:id')
+  acceptRequest(@User() user: AuthModel, @Param('id') receiverId: string) {
+    return this.friendService.acceptRequest(user.sub, parseInt(receiverId));
+  }
+
+  @UseGuards(AuthGuard)
+  @Delete('user/friend/:id')
+  deleteFriend(@User() user: AuthModel, @Param('id') receiverId: string) {
+    return this.friendService.deleteFriend(user.sub, parseInt(receiverId));
   }
 }
--- backend/src/api/friend/friend.service.ts
+++ backend/src/api/friend/friend.service.ts
@@ -14,6 +14,22 @@ import { OracleService } from 'src/oracle/oracle.service';
 export class FriendService {
   constructor(private readonly db: OracleService) {}
 
+  async getMyFriends(me: number) {
+    return await this.db.pool
+      .execute(
+        `
+      SELECT F.PENDING "pending", F.WHEN "when", F.SENDER_ID "senderId", F.RECEIVER_ID "receiverId", S.FULLNAME "senderName", R.FULLNAME "receiverName"
+      FROM FRIENDS F
+      JOIN USERS S ON F.SENDER_ID = S.ID
+      JOIN USERS R ON F.SENDER_ID = R.ID
+      WHERE F.SENDER_ID = :me OR F.RECEIVER_ID = :me
+      `,
+        { me },
+        this.db.jsonFormat,
+      )
+      .then((res) => res.rows);
+  }
+
   async requestFriend(senderId: number, receiverId: number) {
     const res = (await this.db.pool
       .execute(
@@ -41,16 +57,41 @@ export class FriendService {
   }
 
   async acceptRequest(senderId: number, receiverId: number) {
-    const res = (await this.db.pool.execute(`
+    const res = (await this.db.pool
+      .execute(
+        `
       UPDATE FRIENDS SET PENDING = 0
-      WHERE SENDER_ID = :senderId AND RECEIVER_ID = :receiverId
-      `, {senderId, receiverId}, this.db.autoCommit)
-      .then(res => res.rowsAffected == 1)) as boolean;
+      WHERE SENDER_ID = :receiverId AND RECEIVER_ID = :senderId
+      `,
+        { senderId, receiverId },
+        this.db.autoCommit,
+      )
+      .then((res) => res.rowsAffected == 1)) as boolean;
 
-      if (res) {
+    if (res) {
       return { message: 'Friend accepted.' };
     } else {
       throw new NotFoundException('Friend request not found!');
+    }
+  }
+
+  async deleteFriend(senderId: number, receiverId: number) {
+    const res = (await this.db.pool
+      .execute(
+        `
+      DELETE FROM FRIENDS
+      WHERE SENDER_ID = :senderId AND RECEIVER_ID = :receiverId
+      OR SENDER_ID = :receiverId AND RECEIVER_ID = :senderId
+      `,
+        { senderId, receiverId },
+        this.db.autoCommit,
+      )
+      .then((res) => res.rowsAffected > 0)) as boolean;
+
+    if (res) {
+      return { message: 'Friend deleted.' };
+    } else {
+      throw new NotFoundException('Friend not found!');
     }
   }
 }
